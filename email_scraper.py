a21dfdd3f041d3a10a2607fee3aee683060fb718
pedrosoori <61288220+pedrosoori@users.noreply.github.com>
2022-05-31 23:16:44 +0200

    Add files via upload

---
+++ email_scraper.py
@@ -0,0 +1,77 @@
+import imaplib
+import email
+import json
+import time
+
+
+def setup():
+    borrar = open("txt.txt", "w")
+    borrar.write("")
+    borrar.close()
+    
+    user, password = "sooriraffles1@gmail.com", "moqeasdqrwslccqo"
+    
+    imap_url = "imap.gmail.com"
+    my_mail=imaplib.IMAP4_SSL(imap_url)
+    my_mail.login(user, password)
+    
+    my_mail.select("Inbox")
+    _, data=my_mail.search(None, '(FROM "info@service-mail.zalando.es")')
+    
+    mail_id_list=data[0].split()
+    
+    msgs=[]
+    for num in mail_id_list:
+        typ, data = my_mail.fetch(num, '(RFC822)')
+        msgs.append(data)
+    
+    codigos=[]
+    
+    for msg in msgs[::-1]:
+        for response_part in msg:
+            if type(response_part) is tuple:
+                my_msg=email.message_from_bytes((response_part[1]))
+                for part in my_msg.walk():
+                    #print(part.get_content_type())
+                    if part.get_content_type() == 'text/plain':
+                        #print(part.get_payload())
+                        f =open("txt.txt", 'a+')
+                        f.write(part.get_payload())
+                        f.close()
+           
+    
+    for mail in mail_id_list:
+        my_mail.store(mail, '+X-GM-LABELS', '\\Trash')
+    my_mail.expunge()
+    
+    
+    f =open("txt.txt", 'r')
+    datafile = f.readlines()
+    for line in datafile:
+        if '=09[=E2=86=92]' in line:
+            cupon=line.split()
+            codigos.append(cupon[0])
+    #print(codigos)        
+    f.close()
+    
+    datos = {}
+    datos["cupones"]=codigos
+    
+    try:          
+        with open("promocode.json", "r") as f:
+            score = json.load(f)
+            lista=score["cupones"]
+            for cupon in codigos:
+                if cupon not in lista:
+                    lista.append(cupon)
+            score["cupones"]=lista
+        with open("promocode.json", "w") as f:
+            json.dump(score, f)
+    except:
+        with open("promocode.json", "w") as f:
+            json.dump(datos, f)
+            
+# while True:
+#     setup()
+#     print('buscando emails')
+#     time.sleep(10)
